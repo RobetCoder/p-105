16f9e6d8581fcc06d3d986ec6ef707f353a15554
RobetCoder <93022804+RobetCoder@users.noreply.github.com>
2022-08-07 12:35:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format:'png',
+    png_quality: 90
+});
+camera=document.getElementById("camera")
+Webcam.attach('#camera')
+function take_snapshot() {
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML= '<img id-="captured_image src="' +data_uri+'"/>';
+});
+}
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/qWqV_l12bj/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded');
+}
+
+function check(){
+    img= document.getElementById('captured_image');
+    classifier=classify(img, gotResult);
+}
+function gotResult(){
+    if(error){
+        console.error(error);
+    }   else {
+        console.log(results);
+        document.getElementById("result_of_the_family_member_name").innerHTML=results[0].label;
+        document.getElementById("result_accuracy").innerHTML=+ (results[0].confidence*100).toFixed(3) +"%" ;
+ }  
+}
